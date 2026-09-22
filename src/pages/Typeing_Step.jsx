8dbf4518869c4f679b102a9dcb5f2313247e9e99
Fazlahmad <fazlahmadhotaki7@gmail.com>
2026-09-22 21:28:56 +0430

    colorسیبس

--- src/pages/Typeing_Step.jsx
+++ src/pages/Typeing_Step.jsx
@@ -1862,7 +1862,7 @@ export default function Typeing_Step() {
 
                         if (index < typedText.length) {
                           if (typedText[index] === targetText[index]) {
-                            color = "text-green-400";
+                            color = "text-[#3EB489]";
                           } else {
                             color = "text-red-600";
                           }
